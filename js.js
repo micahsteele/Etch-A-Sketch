091042319e165de40a585e65ea9a66c29cc01497
Micah Steele <micahs66@gmail.com>
2024-09-05 13:23:24 +1000

    on hover changes color of a square in the grid

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-const container = document.querySelector("#container")
-console.log(container)
+const container = document.querySelector("#container");
+console.log(container);
 
 for(x = 0; x < 256; x++) {
     const square = document.createElement("div");
@@ -8,4 +8,23 @@ for(x = 0; x < 256; x++) {
 
     container.appendChild(square);
 }
+
+const squares = document.querySelectorAll(".squares");
+console.log(squares);
+
+squares.forEach(div => div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "red";    
+    }))
+
+
+// squares.addEventListener("mouseover", () => {
+//     squares.style.backgroundColor = "red";    
+// })
+
+
+// let mouseOverFunction = function() {
+//     div.style.backgroundColor = "red"
+// };
+
+// squares.onmouseover = mouseOverFunction;
 
