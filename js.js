69e32b42896a5767b727b48771f1b20fb08d0fa3
Micah Steele <micahs66@gmail.com>
2024-09-05 15:18:22 +1000

    reset button generates grid size from button input

--- js.js
+++ js.js
@@ -1,14 +1,25 @@
 const container = document.querySelector("#container");
 console.log(container);
 
-for(x = 0; x < 256; x++) {
-    const square = document.createElement("div");
-    square.classList.add("squares");
-    square.textContent = "hi";
+// for(x = 0; x < 256; x++) {
+//     const square = document.createElement("div");
+//     square.classList.add("squares");
+//     square.textContent = "hi";
 
-    container.appendChild(square);
-}
+//     container.appendChild(square);
+// }
 
+// let generateGrid = function gridGeneration(number) {
+//     for(x = 0; x < 256; x++) {
+//     const square = document.createElement("div");
+//     square.classList.add("squares");
+//     square.textContent = "hi";
+
+//     container.appendChild(square);
+//     }
+// }
+
+const button = document.querySelector(".button");
 const squares = document.querySelectorAll(".squares");
 console.log(squares);
 
@@ -17,14 +28,22 @@ squares.forEach(div => div.addEventListener("mouseover", () => {
     }))
 
 
-// squares.addEventListener("mouseover", () => {
-//     squares.style.backgroundColor = "red";    
-// })
 
-
-// let mouseOverFunction = function() {
-//     div.style.backgroundColor = "red"
-// };
-
-// squares.onmouseover = mouseOverFunction;
-
+button.addEventListener("click", () => {
+    let question = prompt("Enter desired heigth x width. Cannot exceed 100");
+    if (question > 100) {
+        alert("entered too high of a number")
+        return
+    }
+    let gridSize = question * question
+    squares.forEach(div => {
+        div.style.backgroundColor = "transparent";    
+    })
+    for(x = 0; x < gridSize; x++) {
+        const square = document.createElement("div");
+        square.classList.add("squares");
+        square.textContent = "hi";
+    
+        container.appendChild(square);
+    }
+})
