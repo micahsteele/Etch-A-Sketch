bf2abe5904d62d2a322d26751f480340ceac32d0
Micah Steele <micahs66@gmail.com>
2024-09-05 15:52:44 +1000

    on reset allows divs background color to change

--- js.js
+++ js.js
@@ -1,23 +1,13 @@
 const container = document.querySelector("#container");
 console.log(container);
 
-// for(x = 0; x < 256; x++) {
-//     const square = document.createElement("div");
-//     square.classList.add("squares");
-//     square.textContent = "hi";
+for(x = 0; x < 256; x++) {
+    const square = document.createElement("div");
+    square.classList.add("squares");
+    square.textContent = "hi";
 
-//     container.appendChild(square);
-// }
-
-// let generateGrid = function gridGeneration(number) {
-//     for(x = 0; x < 256; x++) {
-//     const square = document.createElement("div");
-//     square.classList.add("squares");
-//     square.textContent = "hi";
-
-//     container.appendChild(square);
-//     }
-// }
+    container.appendChild(square);
+}
 
 const button = document.querySelector(".button");
 const squares = document.querySelectorAll(".squares");
@@ -25,20 +15,19 @@ console.log(squares);
 
 squares.forEach(div => div.addEventListener("mouseover", () => {
         div.style.backgroundColor = "red";    
-    }))
-
-
+}))
 
 button.addEventListener("click", () => {
+    container.replaceChildren();
     let question = prompt("Enter desired heigth x width. Cannot exceed 100");
     if (question > 100) {
         alert("entered too high of a number")
         return
     }
     let gridSize = question * question
-    squares.forEach(div => {
-        div.style.backgroundColor = "transparent";    
-    })
+    // squares.forEach(div => {
+    //     div.style.backgroundColor = "transparent";    
+    // })
     for(x = 0; x < gridSize; x++) {
         const square = document.createElement("div");
         square.classList.add("squares");
@@ -46,4 +35,13 @@ button.addEventListener("click", () => {
     
         container.appendChild(square);
     }
+    const squares = document.querySelectorAll(".squares");
+    console.log(squares);
+
+    squares.forEach(div => div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "red";    
+    }))
+
 })
+
+
