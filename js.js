c99ff77833f06e0c07490608c3d9cda57fae86e6
Micah Steele <micahs66@gmail.com>
2024-09-09 08:13:30 +1000

    randomizes color, fits 1 between (100^2) on screen

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ console.log(container);
 for(x = 0; x < 256; x++) {
     const square = document.createElement("div");
     square.classList.add("squares");
-    square.textContent = "hi";
+    square.textContent = "";
 
     container.appendChild(square);
 }
@@ -34,7 +34,7 @@ button.addEventListener("click", () => {
     for(x = 0; x < gridSize; x++) {
         const square = document.createElement("div");
         square.classList.add("squares");
-        square.textContent = "hi";
+        square.textContent = "";
     
         container.appendChild(square);
     }
